95c0b45b4202e3cafe950211e826594c069dc129
trustlelab <trustlelab@gmail.com>
2023-10-18 08:59:48 +0200

    fix: Resolved build issues

--- webapp/pages/upload/index.tsx
+++ webapp/pages/upload/index.tsx
@@ -227,6 +227,7 @@ export default function UploadPage(props: InitialProps){
                 return (
                     <Collapse
                         size="large"
+                        key={idx}
                         items={[
                             {
                             key: idx,
